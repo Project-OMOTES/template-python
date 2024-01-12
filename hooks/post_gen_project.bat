a7b9aa1ff333e88a9d35d9a76a8cf4df669be0c0
michieltukker <michiel.tukker@deltares.nl>
2024-01-12 10:48:55 +0100

    fixed incorrect paths in post_gen hook for win32

--- hooks/post_gen_project.bat
+++ hooks/post_gen_project.bat
@@ -1,7 +1,7 @@
 
 echo "Creating virtual environment for development in new directory."
-.\ci\linux\create_venv.bat
+.\ci\win32\create_venv.bat
 echo "Updating dependencies"
-.\ci\linux\update_dependencies.bat
+.\ci\win32\update_dependencies.bat
 echo "Installing dependencies"
-.\ci\linux\install_dependencies.bat
+.\ci\win32\install_dependencies.bat
