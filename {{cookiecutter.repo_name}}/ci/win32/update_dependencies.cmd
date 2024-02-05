3045505040c0375c7e9d72f2a464b52c39ded4b9
michieltukker <michiel.tukker@deltares.nl>
2024-02-05 10:25:54 +0100

    Added (generated) requirements.txt as dependency
    Ensures that the package versions are identical between production and dev environments

--- {{cookiecutter.repo_name}}/ci/win32/update_dependencies.cmd
+++ {{cookiecutter.repo_name}}/ci/win32/update_dependencies.cmd
@@ -2,5 +2,5 @@
 pushd .
 cd /D "%~dp0"
 pip-compile --output-file=..\..\requirements.txt ..\..\pyproject.toml
-pip-compile --extra=dev --output-file=..\..\dev-requirements.txt ..\..\pyproject.toml
+pip-compile --extra=dev --output-file=..\..\dev-requirements.txt -c requirements.txt  ..\..\pyproject.toml
 popd
