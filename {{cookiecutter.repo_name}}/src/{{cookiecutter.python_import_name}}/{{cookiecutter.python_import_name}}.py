42ba9fe682c564f6c211fef68f09307ae5d7c0e8
michieltukker <michiel.tukker@deltares.nl>
2024-02-15 17:39:45 +0100

    Fixed linting errors in template script

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.python_import_name}}/{{cookiecutter.python_import_name}}.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.python_import_name}}/{{cookiecutter.python_import_name}}.py
@@ -17,14 +17,15 @@
 from datetime import datetime, timedelta
 from app_logging import setup_logging, LogLevel
 import logging
-import traceback 
+import traceback
 
 logger = logging.getLogger(__name__)
+
 
 def testable_function(input: datetime):
     """Testable function"""
     return input + + timedelta(hours=1)
-    
+
 
 def start_app(loglevel: str, colors: bool) -> None:
     """{{cookiecutter.project_name}} application"""
@@ -32,12 +33,13 @@ def start_app(loglevel: str, colors: bool) -> None:
         setup_logging(LogLevel.parse(loglevel), colors)
 
         logger.info(f"The time + 1 hour is: {testable_function(datetime.now())}")
-        
+
         # TODO insert your code here
 
     except Exception as error:
         logger.error(f"Error occured: {error} at: {traceback.format_exc(limit=-1)}")
         logger.debug(traceback.format_exc())
+
 
 if __name__ == '__main__':
     start_app(loglevel="DEBUG", colors=True)
