aff6119fb6b1632f7d1e91c2114cdc66abf6a3ad
samvanderzwan <sam.vanderzwan@deltares.nl>
2024-09-17 10:59:15 +0200

    Removed license reference from python files

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.python_import_name}}/{{cookiecutter.python_import_name}}.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.python_import_name}}/{{cookiecutter.python_import_name}}.py
@@ -1,18 +1,3 @@
-#  Copyright (c) 2024 Deltares.
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 """Main python file for {{cookiecutter.project_name}}."""
 from datetime import datetime, timedelta
 from {{cookiecutter.python_import_name}}.app_logging import setup_logging, LogLevel
